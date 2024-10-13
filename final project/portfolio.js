749566060b1d6800a09f53952f8179975a72d0b9
Ubaidahmed786 <ubaidahmed806@gmail.com>
2024-10-14 00:56:09 +0500

    Add files via upload
    https://finalprojectmadebyubaidahmed.netlify.app/

---
+++ final project/portfolio.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const navLinks = document.getElementById('navLinks'); 
+    navLinks.classList.toggle('active'); 
+}
+function openWhatsApp() {
+ 
+    const whatsapp = "https://wa.me/923042440396";
+    window.open(whatsapp, '_blank'); 
+}
